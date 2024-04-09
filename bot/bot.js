7be8af9fa930ee8527c1f6eafeb7a8b0fb2fdd16
dan.kudlai <dan.kudlai@techfabric.com>
2024-04-09 18:02:03 +0200

    set up an Express server in your project with a single route to respond to web pings.

--- bot/bot.js
+++ bot/bot.js
@@ -1,4 +1,15 @@
 
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Bot is running');
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Web server is running');
+});
+
 require('./events/commands/start.event');
 require('./events/base/callbackQuery.event')
 require('./events/base/message.event')
